00e69bedb44f699a787dae51b0600670ac5740d2
opilane <opilane@HP-CZC2349HTD>
2023-10-06 10:48:56 +0300

    added checkboxes and radioboxes

--- TreeForm.cs
+++ TreeForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MyFirstForm
 {
@@ -34,8 +35,15 @@ namespace MyFirstForm
             btn.MouseHover += button2_MouseHover;
             tree.MouseDoubleClick += button_MouseDOubleClick;
 
+            treeNode.Nodes.Add(new TreeNode("Tekstkast_Textbox"));
+            txt_box=new TextBox();
+            txt_box.BoederStyle = BorderStyle.Fixed3D;
+            txt_box.Height = 50;
+            txt_box.Width = 100;
+            txt_box.Text = "...";
 
-            tree.Nodes.Add(new TreeNode("Silt-Label"));
+
+            tree.Nodes.Add(new TreeNode("Radionup_Radiobutton"));
             lbl = new Label();
             lbl.Text = "Pealkiri";
             lbl.Location = new Point(tree.Width, 0);
@@ -54,19 +62,54 @@ namespace MyFirstForm
             this.Controls.Add(txt);
             txt.Visible = true;
             treeNode.Nodes.Add(new TreeNode("Radionupp"));
-            r1 = new RadioButton();
-            r1.Text = "VAlik";
-            r1.Visible = true;
-            r1.Location = new Point(tree.Width, txt.Location.Y + txt.Height);
-            r2 = new RadioButton();
-            r2.Text = "valik2";
-            r2.Visible = true;
-            r2.Location = new Point(tree.Width, c1.Location.Y + c1.Height);
+
+            r2.CheckedChanged += new EventHandler(RadioButtons_Changed);
+            this.Controls.Add(r1);
+            this.Controls.Add(r2);
+            c1 = new CheckBox();
+            c1.Text= "Näita nupp";
+            c1.Location = new Point(tree.Width, r1.Location.Y + r1.Height);
             this.Controls.Add(c1);
 
-            tree.Nodes.Add(treeNode);
-            this.Controls.Add(tree);
+            c1.CheckedChanged += new EventHandler(C1_CheckedChanged);
+            c2 = new CheckBox();
 
+
+            c2.Text = "Naita veel midagi";
+
+            c2.Location = new Point(tree.Width, c1.Location.Y + c1.Height);
+            this.Controls.Add(c2);
+
+
+            c2.CheckedChanged += new EventHandler(C1_CheckedChanged);
+            // Image
+
+            pb = new PictureBox();
+            pb.Location = new Point(tree.Width, c2.Location.Y + c2.Height);
+            pb.Image = new Bitmap("../../../sipsik.png");
+            pb.Size = new Size(100, 100);
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            pb.BorderStyle = BorderStyle.Fixed3D;
+            this.Controls.Add(pb);
+
+            //r1 = new RadioButton();
+            //r1.Text = "VAlik";
+            //r1.Visible = true;
+            //r1.Location = new Point(tree.Width, txt.Location.Y + txt.Height);
+            //r2 = new RadioButton();
+            //r2.Text = "valik2";
+            //r2.Visible = true;
+            //r2.Location = new Point(tree.Width, c1.Location.Y + c1.Height);
+            //this.Controls.Add(c1);
+
+            //tree.Nodes.Add(treeNode);
+            //this.Controls.Add(tree);
+
+        }
+
+        private void C1_CheckedChanged(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void RadioButtons_Changed(object? sender, EventArgs e)
@@ -135,6 +178,19 @@ namespace MyFirstForm
                 else if (e.Node.Text == "Silt-Label")
                 {
                     this.Controls.Add(lbl);
+                }
+            }
+        }
+
+        private void Txt_box_KeyDown(object? sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                DialogResult resul = MessagesBox.Show("Kas sa oled kindel?", "Küsimus", MessageBoxButtons.YesNo);
+                if(result == DialogResult.Yes) 
+                {
+                    txt_box.Enabled = false;
+                    this.Text = Txt_box_KeyDown().Text; 
                 }
             }
         }
